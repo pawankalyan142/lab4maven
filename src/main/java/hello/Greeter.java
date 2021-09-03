1c065e536055e9b1d27125c224e7b0a500fe235b
mdmustafa-77 <74002048+mdmustafa-77@users.noreply.github.com>
2021-09-03 18:31:45 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Venkata Naresh Mandhala your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
+    return "Hello world- This is md mustafa and Venkata Naresh Mandhala is my Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
 }
