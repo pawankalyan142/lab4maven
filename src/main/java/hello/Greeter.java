59b73e73eb18c9891861de7e02251da3e381c832
pawankalyan142 <74000594+pawankalyan142@users.noreply.github.com>
2021-09-04 08:59:04 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,6 +1,7 @@
 package hello;
 public class Greeter{
   public String sayHello() {
-    return "Hello world- This is md mustafa and Venkata Naresh Mandhala is my Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
+    return "Hello world- This is B.Pawankalyan from sec-2 
+      !! Hi or Bye is the string needed here.Testing Pipeline";
   }
 }
